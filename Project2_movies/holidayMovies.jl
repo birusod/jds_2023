1168185b43bbe4463aaafe39fb5a61426990857f
birusod <birusod@gmail.com>
2023-12-16 07:44:57 -0800

    project2 final commit

--- Project2_movies/holidayMovies.jl
+++ Project2_movies/holidayMovies.jl
@@ -7,9 +7,10 @@
 using Downloads, Dates, Statistics
 
 ## Loaded
-using CSV, DataFrames, CategoricalArrays
-using AlgebraOfGraphics, CairoMakie
+using CSV, DataFrames
 using Tidier, Chain
+#using AlgebraOfGraphics, CairoMakie
+
 
 # Load Data ===============================================
 
@@ -33,7 +34,7 @@ dd = @chain ddd begin
     @mutate(time = as_integer(time))
 end
 
-parse.(Int64, dd.time[1:2])
+#parse.(Int64, dd.time[1:2])
 # EDA ======================================================
 
 
@@ -54,6 +55,12 @@ gdf = @chain dd begin
     # @mutate(rowid = row_number()) *not needed if a idcol is present
     @pivot_longer(g1:g3, names_to = "group", values_to = "genres")  # -rowid
 end
+
+ff = DataFrame(a = ["1-1", "2-2", "3-3-3"]);
+@separate(ff, a, [b, c, d], "-")
+@select(ff, a)
+
+
 
 @chain gdf begin
     @count(genres, sort = true)
@@ -108,7 +115,7 @@ avg_time = avgfunc(avg_df, :genres, :time)
 
 # Viz =======================================================
 
-# AlgebraOfGraphics + CairoMakie
+using AlgebraOfGraphics, CairoMakie
 set_aog_theme!()
 update_theme!(fontsize=30, markersize=40, Axis=(title="MyDefaultTitle",))
 #axis = (width = 225, height = 225)
@@ -121,46 +128,190 @@ draw(
 
 ## by type
 bytype
+plt_barplot = data(bytype) *
+    mapping(
+        :type,
+        :total;
+        color=:type) *
+visual(BarPlot)
+draw(plt_barplot)
 
-plot(
-    bytype,  # Use the @df macro to refer to the DataFrame
-    mapping=:type => :total,  # Specify the mapping of variables
-    Geom.bar,  # Use the bar geometry
-    Scale.x_discrete,  # Use discrete scale for x-axis
-    
-)
 
-plot(
-    result,
-    mapping=:Group => :rounded_mean_summary,  # Specify the mapping of variables
-    Geom.bar,  # Use the bar geometry
-    Scale.x_discrete,  # Use discrete scale for x-axis
-    Guide.xlabel("Group"),  # Label for x-axis
-    Guide.ylabel("Rounded Mean Value")  # Label for y-axis
-)
+draw(
+    data(bytype) *
+    mapping(
+        :type, :total;
+        color=:type) *
+    visual(BarPlot);
+    axis=(title="Distribution By Movie Type",)
+    )
 
 
 
 # by  genres
+gdf5 = first(gdf10, 5)
+labels = ["Comedy", "Drama", "Romance", "Family", "Other"]
+draw(
+    data(gdf5) *
+    mapping(
+        :genres, :n;
+        color = :genres => sorter(labels)) *
+    visual(BarPlot);
+    axis=(;
+        title = "Top 5 Most Frequent Genres",
+        titlealign = :left, # using Makie.jl's symbols
+        titlecolor = :blue, # using Makie.jl's symbols
+        titlefont = "DejaVu Sans Mono",
+        titlegap = 30,      # the gap between title and the plot
+        titlesize = 32,
+        ),
+)
+
+gdf10[!, :odr] = collect(1:10)
 gdf10
+
+draw(
+    data(gdf10) *
+    mapping(:odr => "", :n => "", color = :genres  => "Genre") *
+    visual(BarPlot; width = .5),
+    axis = (
+        title  =  "Top 10 Most Frequent Genre",
+        xticklabelrotation=45.0,
+        xticklabelcolor = :white,
+        xtickcolor = :white
+    ),
+    legend = (; position = :top, titleposition = :left)
+)
 
 
 ## By year
 byyear
 
+draw(
+    data(byyear) *
+    mapping(
+        :year, :n) *
+    visual(BarPlot, color = (:firebrick),);
+    axis=(
+        title = "Distribution Of Movies By  Year",
+        titlealign = :center, # using Makie.jl's symbols
+        titlecolor = :firebrick, # using Makie.jl's symbols
+        titlefont = "Roboto",
+        titlegap = 100,      # the gap between title and the plot
+        titlesize = 20,)
+)
 
 ## time
 avg_df
+duration = filter(row -> row.time < 150, dropmissing(avg_df))
+duration
+draw(
+    data(duration) * 
+    mapping(:time => "Duration in minutes") * 
+    histogram(bins=30);
+    axis = (title = "Histogram of Duration",)
+)
 
+
+
+using AlgebraOfGraphics: density
+mycolors = ["#FC7808", "#8C00EC", "#107A78"]
+draw(
+    data(duration) * 
+    mapping(:time => "Duration in minutes") * 
+    density();
+    axis = (
+        title = "Density of Duration",)
+)
+
+
+# votes vs rating
+vr = filter(r -> r.votes < 8000 && r.votes > 2000, dd)
+draw(
+    data(vr) * 
+    mapping(:votes, :rating, color = :type) * 
+    visual(Scatter),
+    axis = (title = "Scatterplot: Votes vs Rating By  Type",),
+    palettes = (color =  mycolors,)
+)
+
+
+# type: boxplot
+data(dd) * 
+    mapping(:type => "", :rating, color = :type => (t -> "Type " * t ) => "TYPES") * 
+    visual(BoxPlot) |> draw
+
+# type: violin side = :v_cat, color = :v_cat
+with_theme(
+    theme_minimal(); 
+    Axis = (; 
+    bottomspinecolor = :red, 
+    leftspinecolor = :blue,
+    title = "Rating Distribution By Type",
+    titlecolor = :firebrick, # using Makie.jl's symbols
+    titlefont = "Roboto",
+    titlesize = 30)
+    ) do
+    data(dd) * 
+    mapping(
+        :type => "", 
+        :rating => "Average Rating", 
+        color = :type => "TYPE") * 
+    visual(Violin) |> draw
+end
 
 
 # rating by genre (top10)
+with_theme(
+    theme_minimal(); 
+    Axis = (; 
+    title = "Rating By Movie Genre",
+    bottomspinecolor = :grey,
+    xticklabelrotation=45.0,
+    xticklabelfont = :bold,
+    titlecolor = :dodgerblue, 
+    titlesize = 30)
+    ) do
+    data(avg_rtg) * 
+    mapping(:genres => "", :avg  => "Average") * 
+    visual(BarPlot) |> draw
+end
 
-avg_rtg
 
+# votes by genre (top10)
 avg_votes
+with_theme(
+    theme_minimal(); 
+    Axis = (; 
+    title = "Votes By Movie Genre",
+    bottomspinecolor = :grey,
+    xticklabelrotation=45.0,
+    xticklabelfont = :bold,
+    titlecolor = :crimson, 
+    titlesize = 30)
+    ) do
+    data(avg_votes) * 
+    mapping(:genres => "", :avg  => "Average") * 
+    visual(BarPlot) |> draw
+end
 
-avg_time
+
+
+# Time by genre (top10)
+with_theme(
+    theme_minimal(); 
+    Axis = (; 
+    title = "Duration By Movie Genre",
+    bottomspinecolor = :grey,
+    xticklabelrotation=45.0,
+    xticklabelfont = :bold,
+    titlecolor = :darkgreen, 
+    titlesize = 30)
+    ) do
+    data(avg_time) * 
+    mapping(:genres => "", :avg  => "Average") * 
+    visual(BarPlot) |> draw
+end
 
 
 
